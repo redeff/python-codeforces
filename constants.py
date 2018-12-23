fa0e6c5c161e012562e3748ab12bc49875926c93
reedef <14carlosoto@gmail.com>
2018-12-23 22:18:03 +0000

    separated constants

---
+++ constants.py
@@ -0,0 +1,15 @@
+
+# These are random numbers. It doesn't matter what they are,
+# only that they're constant
+
+# 18-digit base-36 random number
+ftaa = 'bnh5yzdirjinqaorq0'
+
+# 32-digit base-16 random number
+bfaa = '1a55b95112dd900a28d64e2d0f841341'
+
+
+# Page constants
+
+page = 'https://codeforces.com'
+page_http = 'http://codeforces.com'
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python3
 import requests, re, json, sys, os
-
-# These are random numbers. It doesn't matter what they are,
-# only that they're constant
-
-# 18-digit base-36 random number
-ftaa = 'bnh5yzdirjinqaorq0'
-
-# 32-digit base-16 random number
-bfaa = '1a55b95112dd900a28d64e2d0f841341'
-
-page = 'https://codeforces.com'
-page_http = 'http://codeforces.com'
+import constants
 
 def pre_auth():
     r = requests.get(page + '/enter');
